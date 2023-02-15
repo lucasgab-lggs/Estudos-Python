e9add66540511ac0aa3d7c31f3061ebcb1f2fa88
Lucas Gabriel <lucasgab.lggs@gmail.com>
2023-02-15 08:05:14 -0300

    Adivinhação de palavras.

--- adivinhador.py
+++ adivinhador.py
@@ -1,15 +1,19 @@
+'''Importação de bibliotecas necessárias para o funcionamento do jogo.'''
 import random
 
+'''Variáveis iniciais para controle do jogo.'''
 tentativas = 0
 palpite = 0
 lim_inferior = int(input("Digite o limite inferior: "))
 lim_superior = int(input("Digite o limite superior: "))
 numero = random.randint(lim_inferior, lim_superior)
 
+'''Roda um loop até que o usuário acerte o número.'''
 while True:
     palpite = int(input("Digite um número para seu palpite: "))
     tentativas += 1
 
+    '''Verifica se os palpites são iguais, menores ou maiores do que o número randomizado.'''
     if palpite == numero:
         print("Parabéns você acertou o número em ", tentativas, " tentativas.")
         break
---
+++ adivinhador_palavras.py
@@ -0,0 +1,43 @@
+'''Importação de bibliotecas necessárias para o funcionamento do jogo.'''
+import random
+
+'''Variáveis iniciais para controle do jogo.'''
+palavras = ["casa", "cachorro", "carro", "elefante",
+            "computador", "ciencia", "vidro", "luz",
+            "biblioteca", "tesoura", "secador", "mesa"]
+palavra = random.choice(palavras)
+tentativas = ""
+turnos = 12
+
+print("Descubra as letras da palavra e revele-a!")
+
+'''Loop até que o usuário esgote as 12 tentativas'''
+while turnos > 0:
+
+    '''Contador que indica a quantidade de letras não descobertas'''
+    restantes = 0
+
+    '''Loop para impressão (ou não) das letras que já foram descobertas'''
+    for letra in palavra:
+        if letra in tentativas:
+            print(letra)
+        else:
+            print("-")
+            restantes += 1
+    
+    '''Verifica se o usuário descobriu todas as letras que tinha para descobrir'''
+    if restantes == 0:
+        print("Você venceu!")
+        print("A palavra era:", palavra)
+    
+    '''Recebimento de entrada para o palpite do usuário'''
+    tentativa = input("Qual letra? ")
+    tentativas += tentativa
+
+    '''Verifica se a letra digitada NÃO se encontra na palavra escolhida'''
+    if tentativa not in palavra:
+        turnos -= 1
+        print("Errado!")
+        print("Você tem mais", turnos, "tentativas!")
+        if turnos == 0:
+            print("Você perdeu!")
